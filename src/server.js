2ada747316926b97674417d42acf46e2f3675536
KyleSzymanski41 <kyleszymanski@gmail.com>
2016-01-31 19:17:55 -0500

    working on ip address issue

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ var app = express();
 // Config
 var port = 3000;
 
-app.use('/api', require('../routes/api.js')(express));
-
 var server = app.listen(port, function() {
     console.log('Server Active on', port);
 });
+
+app.use('/api', require('../routes/api.js')(express));
 
 module.exports = server;
